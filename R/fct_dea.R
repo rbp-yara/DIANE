cfb803264730e8ecf40ab9b11e81f120ad1eb7bb
Alexandre <soriano.alexandre30@gmail.com>
2023-12-11 17:10:38 +0100

    Fix a bug with normalization factor

--- R/fct_dea.R
+++ R/fct_dea.R
@@ -39,7 +39,7 @@ estimateDispersion <- function(tcc, conditions = NULL) {
   }
   else{
     groups <- conditions
-  }
+  },
   design = stats::model.matrix( ~ groups + 0)
   rownames(design) <- colnames(tcc$count)
   colnames(design) <-
@@ -48,8 +48,8 @@ estimateDispersion <- function(tcc, conditions = NULL) {
 
   dge <- edgeR::DGEList(
     counts = tcc$count,
-    lib.size = tcc$norm.factors,
-    norm.factors = colSums(tcc$count),
+    lib.size =  colSums(tcc$count),
+    norm.factors = tcc$norm.factors,
     group = groups,
     genes = rownames(tcc$count)
   )
